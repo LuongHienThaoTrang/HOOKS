2bd6fbaf0d4646254029b956be5258e0b116cdd4
Luong Hien Thao Trang <luonghienthaotrang2906@gmail.com>
2022-08-11 12:55:52 +0700

    Ex 10: Mounted & Unmounted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import UseState from "./useState/ex9";
+import { useState } from 'react';
+import Content from './useState/ex10';
 
 
 function App() {
+  const [show, setShow] = useState(false)
   
+  const handleToggle = () => {
+    setShow(!show)
+  }
+
   return (
     <div className="App" style={{ padding: 50 }}>
-      <UseState />
+      <button onClick={handleToggle}>Toggle</button>
+      {show && <Content />}
     </div>
   );
 }
---
+++ src/useState/ex10.js
@@ -0,0 +1,11 @@
+
+
+function Content() {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    )
+}
+
+export default Content
