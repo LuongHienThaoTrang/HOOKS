b9e89ac718fd86578aa850ecd72bb0d75a3021d8
Luong Hien Thao Trang <luonghienthaotrang2906@gmail.com>
2022-08-13 15:13:27 +0700

    Ex 3: count with Redux createStore (mapStateToProps, mapDispatchToProps)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 
 import { useState, useEffect, useLayoutEffect, useRef, memo, useCallback, useMemo, useReducer } from "react";
-import UseReducer from "./useReducer/ex2";
-
+import UseReducer from "./useReducer/ex3";
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
 
 function App() {
 
+  const initState = {
+    count: 0
+  }
+
+  const rootReducer = (state = initState, action) => {
+    switch (action.type) {
+      case 'DOWN_ACTION':
+        return {
+          ...state,
+          count: state.count - action.payload
+        }
+      case 'UP_ACTION':
+        return {
+          ...state,
+          count: state.count + action.payload
+        }
+      default:
+        return state
+    }
+  }
+
+  const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(logger, thunk))
+  )
+
   return (
-    <div className="App" style={{ padding: 50 }}>
-      <UseReducer />
-    </div>
+    <Provider store={store}>
+      <div className="App" style={{ padding: 50 }}>
+        <UseReducer />
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/useReducer/ex3.js
@@ -0,0 +1,36 @@
+
+import { useState, useEffect, useLayoutEffect, useRef, memo, useCallback, useMemo, useReducer } from "react";
+import { connect } from "react-redux"
+
+
+function UseReducer({ count, downAction, upAction }) {
+
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={() => downAction()}>Down</button>
+            <button onClick={() => upAction()}>Up</button>
+        </div>
+    )
+}
+
+const mapStateToProps = state => ({
+    count: state.count
+})
+
+const mapDispatchToProps = dispatch => ({
+    downAction: () => {
+        dispatch({
+            type: 'DOWN_ACTION',
+            payload: 1
+        })
+    },
+    upAction: () => {
+        dispatch({
+            type: 'UP_ACTION',
+            payload: 1
+        })
+    }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(UseReducer)
